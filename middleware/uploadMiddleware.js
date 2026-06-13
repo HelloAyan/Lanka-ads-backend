e920c602e8781d7ecb77c62fb6f96d2d6ddad3c7
HelloAyan <helloayan171@gmail.com>
2026-06-13 14:41:21 +0600

    updated uploadMiddleware

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,9 +1,16 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(process.cwd(), "uploads", "ads");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/ads");
+        cb(null, uploadDir);
     },
 
     filename: (req, file, cb) => {
